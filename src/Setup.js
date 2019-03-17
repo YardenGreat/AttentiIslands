2ef9d31d2490643e3d687efb83befed7e53e092e
Yarden <yarden2100@gmail.com>
2019-03-18 00:16:05 +0200

    Cosmetic changes

--- src/Setup.js
+++ src/Setup.js
@@ -1,6 +1,7 @@
 "use strict";
-
 var board, boardWidth, boardHeight;
+
+
 function getSquare(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -54,10 +55,10 @@ function restart(){
 
 function enableDraw(){
     document.getElementById("drawButton").classList.add("notShowing");
-    document.getElementById("instructionsDiv").innerHTML =
+    document.getElementById("instructionsDiv").innerHTML = "<pre>" +
      "Please Press any squares on the board to paint them black. \
-     If you change your mind about a certain square, press it again to repaint it white. \
-     When you're finished, press the 'Solve' button."
+     \nIf you change your mind about a certain square, press it again to repaint it white. \
+     \nWhen you're finished, press the 'Solve' button." + "</pre>"
     canvas.addEventListener('click', function(evt) {
         var mousePos = getSquare(canvas, evt);
         toggleSquare(context, mousePos.x, mousePos.y)
@@ -69,52 +70,7 @@ function handleInputDiv(){
     document.getElementById("solveButton").classList.remove("notShowing");
     document.getElementById("randomizeButton").classList.add("notShowing");
     document.getElementById("drawButton").classList.add("notShowing");
-    // document.getElementById("instructionsDiv").classList.add("notShowing");
 }
 
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-
-
-// drawGrid(context);
-
-
-
-
-
-//--------------------------------------------------------------------
-//Randomize
-/*function randomize(canvas, context, boardWidth, boardHeight){
-    // var rect = canvas.getBoundingClientRect();
-    var normalizedX, normalizedY;
-    board = new Array(boardWidth);
-
-    for (var x = 0.5; x < boardWidth * 10; x += 10) {
-        normalizedX = normalize(x);
-        board[normalizedX] = new Array(boardHeight);
-        for (var y = 0.5; y < boardHeight * 10; y += 10) {
-            normalizedY = normalize(y);
-            if (Math.round(Math.random()) == 1)
-            {
-                //debugger;
-                board[normalizedX][normalizedY] = "black";
-                //squareX = y + 0.5
-                //squareY = x + 0.5;
-                colorSquare(context, y + 0.5, x + 0.5);
-            }
-            else
-            {
-                board[normalizedX][normalizedY] = "white";
-            }        
-        }
-    }
-}
-
-function colorSquare(context, x, y){ 
-    context.fillStyle = "#000000";
-    context.fillRect(x,y,9,9);
-}
-
-function normalize(x){
-    return ((x - 0.5) / 10);
-}*/
